7667fd317bf53acdb3bfff3d05924dffe2cd78e9
Sho Kagaya <drums.sho@gmail.com>
2020-09-24 23:56:39 +0900

    add include file

--- htdocs/company.php
+++ htdocs/company.php
@@ -1,107 +1,113 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>fon光</title>
-<meta name="robots" content="noindex" />
 <meta name="viewport" id="viewport" content="width=device-width">
-<meta name="description" content="">
-<meta name="keywords" content="">
-<!--<link rel="stylesheet" href="css/style.css">--> 
+<meta name="description" content="月額3,980円！fon光で快適なインターネット生活を送ろう">
+<meta name="keywords" content="fon,fon光,nuro,nuro光,NTT,プロバイダ,高速,2Gbps,WiFi,ルーター,WiMAX,Softbank,縛りなしWiF">
+<?php include "include/ogp.html";?>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/other.css"> 
 
 <!--//* JS読み込み *//-->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.cookie.js"></script>
-<script type="text/javascript" src="js/jquery.layerBoard.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
 </head>
 <body>
+<?php include "include/header.html";?>
 <main>
-        <section id="other">
-            <article class="company">
-				<h2>会社概要／特定商取引法に基づく表記</h2>
-				<div class="company_box">
-					<dl>
-						<dt>会社名</dt>
-						<dd>フォン・ジャパン株式会社</dd>
-					</dl>
-					<dl>
-                            <dt>設立年月日</dt>
-                            <dd>2006年8月10日</dd>
-                    </dl>
-                        <dl>
-                            <dt>代表取締役</dt>
-                            <dd>横田 和典</dd>
-                        </dl>
-                        <dl>
-                            <dt>事業者区分</dt>
-                            <dd>電気通信事業：A-18-9032
-                                <br> 届出年月日：平成18年11月29日
-                            </dd>
-                        </dl>
-                        <dl>
-                            <dt>電話番号</dt>
-                            <dd>0120-966-486</dd>
-                        </dl>
-                        <dl>
-                            <dt>メールアドレス</dt>
-                            <dd></dd>
-                        </dl>
-                        <dl>
-                            <dt>所在地</dt>
-                            <dd>〒171-0014 東京都豊島区池袋2-14-4 池袋TAビル8F</dd>
-                        </dl>
-                        <dl>
-                            <dt>お支払い方法</dt>
-                            <dd>クレジットカード・口座振替</dd>
-                        </dl>
-                        <dl>
-                            <dt>商品代金以外の必要料金</dt>
-                            <dd></dd>
-                        </dl>
-                        <dl>
-                            <dt>申し込みの有効期限</dt>
-                            <dd>お申込み内容に不備があり、1ヶ月以上連絡がつかない場合はキャンセルとさせていただく場合がございます。</dd>
-                        </dl>
-                        <dl>
-                            <dt>キャンセル</dt>
-                            <dd>お申し込み時刻の翌14時までのご連絡がキャンセル可能です。</dd>
-                        </dl>
-                        <dl>
-                            <dt>不良品</dt>
-                            <dd>初期不良は端末到着から7日以内のご対応となります。</dd>
-                        </dl>
-                        <dl>
-                            <dt>引き渡し時期</dt>
-                            <dd>
-                                最短翌日発送致します。
-                                <br> 在庫状況やお申込み状況によっては翌日以降の発送となる場合があります。
-								<br>口座振替の場合はお口座情報のご登録が完了後に発送となります。
-								<br>完了まで数日～1週間程度お時間が掛かりますので予めご了承下さいませ。
-                            </dd>
-                        </dl>
-                        <dl>
-                            <dt>お支払い期限</dt>
-                            <dd>クレジットカード決済の場合はお申し込み完了時に初回料金の事務手数料・日割り料金が即日決済となります。<br>
-								翌月以降は毎月27日に当月ご利用代金を決済致します。<br>
-								※お引き落としのタイミングはカード会社ごとに異なります。<br>
-								<br>
-								口座振替の場合は代引きでの発送となります。<br>
-								端末お受取り時に初月ご利用分・事務手数料・初期費用・代引き手数料をお支払いいただきます。<br>
-								翌月以降は毎月27日にご登録の口座より当月ご利用代金をお引き落とし致します。
-                            </dd>
-                        </dl>
-                        <dl>
-                            <dt>契約約款</dt>
-                            <dd>
-                                契約約款につきましては<a href="#" target="_blank">こちら</a>をご確認下さい。
-                            </dd>
-                        </dl>
-					</div>
-            </article>
-        </section>
-    </main>
+    <section id="other">
+        <article class="company">
+            <h2>会社概要／特定商取引法に基づく表記</h2>
+            <div class="company_box">
+                <dl>
+                    <dt>会社名</dt>
+                    <dd>フォン・ジャパン株式会社</dd>
+                </dl>
+                <dl>
+                    <dt>設立年月日</dt>
+                    <dd>2006年8月10日</dd>
+                </dl>
+                <dl>
+                    <dt>代表取締役</dt>
+                    <dd>横田 和典</dd>
+                </dl>
+                <dl>
+                    <dt>事業者区分</dt>
+                    <dd>電気通信事業：A-18-9032
+                        <br> 届出年月日：平成18年11月29日
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>電話番号</dt>
+                    <dd>0120-966-486</dd>
+                </dl>
+                <dl>
+                    <dt>メールアドレス</dt>
+                    <dd></dd>
+                </dl>
+                <dl>
+                    <dt>所在地</dt>
+                    <dd>〒171-0014 東京都豊島区池袋2-14-4 池袋TAビル8F</dd>
+                </dl>
+                <dl>
+                    <dt>お支払い方法</dt>
+                    <dd>クレジットカード・口座振替</dd>
+                </dl>
+                <dl>
+                    <dt>商品代金以外の必要料金</dt>
+                    <dd></dd>
+                </dl>
+                <dl>
+                    <dt>申し込みの有効期限</dt>
+                    <dd>お申込み内容に不備があり、1ヶ月以上連絡がつかない場合はキャンセルとさせていただく場合がございます。</dd>
+                </dl>
+                <dl>
+                    <dt>キャンセル</dt>
+                    <dd>お申し込み時刻の翌14時までのご連絡がキャンセル可能です。</dd>
+                </dl>
+                <dl>
+                    <dt>不良品</dt>
+                    <dd>初期不良は端末到着から7日以内のご対応となります。</dd>
+                </dl>
+                <dl>
+                    <dt>引き渡し時期</dt>
+                    <dd>
+                        最短翌日発送致します。
+                        <br> 在庫状況やお申込み状況によっては翌日以降の発送となる場合があります。
+                        <br>口座振替の場合はお口座情報のご登録が完了後に発送となります。
+                        <br>完了まで数日～1週間程度お時間が掛かりますので予めご了承下さいませ。
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>お支払い期限</dt>
+                    <dd>クレジットカード決済の場合はお申し込み完了時に初回料金の事務手数料・日割り料金が即日決済となります。
+                        <br> 翌月以降は毎月27日に当月ご利用代金を決済致します。
+                        <br> ※お引き落としのタイミングはカード会社ごとに異なります。
+                        <br>
+                        <br> 口座振替の場合は代引きでの発送となります。
+                        <br> 端末お受取り時に初月ご利用分・事務手数料・初期費用・代引き手数料をお支払いいただきます。
+                        <br> 翌月以降は毎月27日にご登録の口座より当月ご利用代金をお引き落とし致します。
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>契約約款</dt>
+                    <dd>
+                        契約約款につきましては<a href="#" target="_blank">こちら</a>をご確認下さい。
+                    </dd>
+                </dl>
+            </div>
+        </article>
+    </section>
+</main>
+<?php include "include/footer.html";?>
 </body>
 </html>
