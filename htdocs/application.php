5559e9d934b90fe188b5d2aea4e26bde8ca1c587
Sho Kagaya <drums.sho@gmail.com>
2020-09-29 12:39:55 +0900

    chnage sp style

--- htdocs/application.php
+++ htdocs/application.php
@@ -256,7 +256,7 @@ $(window).load(function() {
 					<input type="radio" name="remortSupport" value="1" class="check" id="remortSupport">
 					<label for="nuro">あり</label>
 				</li>
-				<li class="categories">ひかりTV for NURO申込</li>
+				<!--li class="categories">ひかりTV for NURO申込</li>
 				<div class="select">
 					<select name="hikariTV" id="hikariTV" class="validate[required]">
 							<option value="0" selected>なし</option>
@@ -265,7 +265,7 @@ $(window).load(function() {
 							<option value="3">ビデオざんまいプラン</option>
 							<option value="4">基本放送プラン</option>
 					</select>
-				</div>
+				</div-->
 			</ul>
 			<div class="documents">
 				<p>入会書類郵送希望先</p>
