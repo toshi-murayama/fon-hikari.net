0bf69f710b4e886f763fa1a115e77c17c628b1ad
ayaka <ayaka0405.182@gmail.com>
2020-12-07 13:33:00 +0900

    summaryやrecommendなどの見せ方変更

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -50,5 +50,61 @@ $(function () {
             } else {
             }
         });
+        $('.animation01').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation02').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation03').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation04').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation05').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation06').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
+        $('.animation07').each(function() {
+            var offset_top = $(this).offset().top,
+                top_margin = 650;
+            if (scroll_top > offset_top - top_margin) {
+                $(this).addClass('fadein');
+            } else {
+            }
+        });
     });
 });
