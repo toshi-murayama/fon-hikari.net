543803849eea5b0c6f22cc136ae93350d3007ad5
Sho Kagaya <drums.sho@gmail.com>
2021-03-10 18:36:27 +0900

    base text commit

---
+++ htdocs/ouchiwari.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>おうち割 | Fon光 超高速光回線インターネット</title>
+<meta name="viewport" id="viewport" content="width=device-width">
+<meta name="description" content="月額4,378円！Fon光で快適なインターネット生活を送ろう">
+<meta name="keywords" content="Fon,Fon光,nuro,nuro光,NTT,プロバイダ,高速,2Gbps,WiFi,ルーター,WiMAX,Softbank,縛りなしWiF">
+<meta name="theme-color" content="#EC7103">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="shortcut icon" href="img/favicon.ico" />
+<?php include "include/ogp.html";?>
+<!--style-->
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+<!--js-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/common.js"></script>
+<script src="js/index.js"></script>
+<script src="js/option.js"></script>
+<!--tag-->
+<?php include "include/tag_head.html";?>
+</head>
+<body>
+<?php include "include/tag_start.html";?>
+<p id="cursor"></p>
+<div id="stalker"></div>
+    <?php
+        if(isset($_COOKIE['affiliate'])) {
+            include "include/header_affiliate.html";
+        } else {
+            include "include/header.html";
+        }
+    ?>
+    <div id="wrap">
+        <section id="title" class="option_img">
+            <article>
+                <h1>おうち割</h1>
+            </article>
+        </section>
+        <section id="ouchiwari">
+            <article>
+                <h2>セットでお得になるオプション</h2>
+                <p>注意事項</p>
+                <ul>
+                    <li>※ 表示価格は税込です。記載の金額を税込総額から割引します。消費税率が変動しても、割引金額は変わりません。</li>
+                    <li>※ 特典は予告なく終了・変更する場合があります。実施状況は担当のオペレーターまでご質問ください。</li>
+                    <li>※ お客さまがソフトバンクで加入されている割引特典によってはおうち割 光セットと併用できない場合があります。</li>
+                    <li>※ NURO 光 でんわ「ホワイトコール24」、ソフトバンクモバイル「ホワイトコール24」両方へのお申し込みが必要です。</li>
+                    <li>※ 本特典は、ソフトバンクにてお申し込み手続きが完了し次第割引開始となり、該当サービスを継続利用している限りは永年割引となります。詳細はソフトバンク窓口にてご確認ください。</li>
+                    <li>※ NURO 光ご契約者本人以外からのお申し込みの場合、専用同意書と家族確認書類が必要です。下記よりダウンロードをしていただき、契約者さまにご記入していただき、ソフトバンク窓口までご持参ください。<a href="">おうち割 光セット専用同意書</a></li>
+                    <li>※ おうち割 光セットのお申し込みは、「NURO 光 でんわ おうち割 光セット確認書」を弊社へご送付の上、NURO 光 でんわ開通後にソフトバンクショップにてお申し込みください。 NURO 光契約者様がソフトバンクショップにお越しいただき、お申し込みの際に『NURO 光 でんわ おうち割 光セット確認書』と『固定電話番号』の情報を申請していただきます様お願いいたします。 NURO 光 でんわのご契約者様ご本人以外からのお申し込みの場合、NURO 光 でんわご契約者様の同意書および、ご契約者様の家族であることを証明できる確認書類が必要となります。</li>
+                    <li>※ 「NURO 光 でんわ おうち割 光セット確認書」はNURO 光 マイページよりダウンロードいただけます。iphone、Softbank スマートフォン、Softbank 3G（携帯電話）、iPad、タブレットのいずれかで指定のデータ（パケット）定額サービスにご加入いただく必要がございます。</li>
+                    <li>※ 法人回線の方もお申し込み可能です。</li>
+                    <li>※ NURO 光 でんわはソニーネットワークコミュニケーションズ株式会社がソフトバンクテレコム株式会社と提携して提供するサービスです。</li>
+                    <li>※ <b>「ソフトバンクの特典期間と内容は予告なく変更する場合があります。 詳細は<a href="">ソフトバンクホームページ</a>をご確認ください。」</b></li>
+                </ul>
+            </article>
+            <artcile>
+                <h3>NURO 光 でんわオプションサービス「おうち割 光セット」</h3>
+                <p>NURO 光 でんわとセットでご利用いただくことで、ご家族でお使いのソフトバンクのスマートフォン、3Gケータイ、またはiPadのご利用料金から最大毎月1,100円割引となるサービスです。</p>
+                <table>
+                    <tr>
+                        <th>データ定額50GB、20GB、5GBの場合</th>
+                        <th>データ定額(3Gケータイ)の場合</th>
+                    </tr>
+                    <tr>
+                        <td>毎月<span>1,100</span>円割引<br>2年間で<em>26,400</em>円割引</td>
+                        <td>毎月<span>550</span>円割引<br>2年間で<em>13,200</em>円割引</td>
+                    </tr>
+                </table>
+                <ul>
+                    <li>※ 3年目以降の割引の有無は、対象固定通信サービスによって異なります。</li>
+                    <li>※ ウルトラギガモンスター定額50GB、データ定額5GB/20GBご利用でお申し込みの場合。割引金額は対象料金サービスにより異なります。</li>
+                </ul>
+                <h4>適用条件</h4>
+                <div>
+                    <dl>
+                        <dt>NURO 光でんわ</dt>
+                        <dd><img src="" alt="NURO 光でんわ"></dd>
+                    </dl>
+                    <div>+</div>
+                    <dl>
+                        <dt>ソフトバンク端末</dt>
+                        <dd><img src="" alt="ソフトバンク端末"></dd>
+                    </dl>
+                    <div>+</div>
+                    <dl>
+                        <dt>ホワイトコール24</dt>
+                        <dd><img src="" alt="ホワイトコール24"></dd>
+                    </dl>
+                    <div>=</div>
+                    <dl>
+                        <dt>毎月1,100円割引★</dt>
+                        <dd><img src="" alt="毎月1,100円割引★"></dd>
+                    </dl>
+                </div>
+                <h5>その他適用条件</h5>
+                <ul>
+                    <li>★ 表示価格は最大の割引額の場合です。</li>
+                    <li>・ NURO 光の契約者名が個人名義であること。</li>
+                    <li>・ 登録する携帯電話がNURO 光の契約者本人、またはご家族名義の契約であること。</li>
+                    <li>・ 割引額はご利用の機種、対象料金サービスにより異なります。</li>
+                    <li>・ NURO 光1契約につき、携帯電話回線最大10回線までの登録であること。</li>
+                </ul>
+                <h4>お申し込み方法</h4>
+                <p>下記の確認書を、確認書１ページに記載の宛先へご送付の上、NURO 光 でんわ開通後に最寄りのソフトバンクショップにてお申し込みいただくようお願い致します。</p>
+                <a href="">専用確認書のダウンロード</a>
+            </artcile>
+        </section>
+
+        <section id="support">
+            <article>
+                <ul>
+                    <li>
+                        <div class="imgBox"><img src="img/support_img01.svg" alt=""></div>
+                        <h3>ご不明な点は</h3>
+                        <p>まずは、よくある質問事項をご確認ください。</p>
+                        <a href="faq">詳しくはこちらから</a>
+                    </li>
+                    <li>
+                        <div class="imgBox"><img src="img/support_img02.png" alt=""></div>
+                        <h3>お困りの場合は</h3>
+                        <p>お困りの問題に対する解決策をご案内します。</p>
+                        <a href="tel:0120-966-486">お電話は0120-966-486まで</a><br>
+                        <a href="contact">メールでのお問い合わせはこちらから</a>
+                    </li>
+                </ul>
+            </article>
+        </section>
+    </div>
+
+    <?php include "include/footer.html";?>
+</body>
+</html>
