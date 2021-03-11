b4b4739eafed7848990dd2c12d5c71f6b11d44b1
Sho Kagaya <drums.sho@gmail.com>
2021-03-11 13:48:32 +0900

    design fix

--- htdocs/ouchiwari.php
+++ htdocs/ouchiwari.php
@@ -38,14 +38,14 @@
         }
     ?>
     <div id="wrap">
-        <section id="title" class="option_img">
+        <section id="title" class="ouchiwari_img">
             <article>
                 <h1>おうち割</h1>
             </article>
         </section>
         <section id="ouchiwari">
             <article>
-                <h2>セットでお得になるオプション</h2>
+                <h2>光電話とセットで家族みんなのスマホが永年<span>毎月最大<em>1,100</em>円おトクに!</span></h2>
                 <p>注意事項</p>
                 <ul>
                     <li>※ 表示価格は税込です。記載の金額を税込総額から割引します。消費税率が変動しても、割引金額は変わりません。</li>
@@ -53,15 +53,15 @@
                     <li>※ お客さまがソフトバンクで加入されている割引特典によってはおうち割 光セットと併用できない場合があります。</li>
                     <li>※ NURO 光 でんわ「ホワイトコール24」、ソフトバンクモバイル「ホワイトコール24」両方へのお申し込みが必要です。</li>
                     <li>※ 本特典は、ソフトバンクにてお申し込み手続きが完了し次第割引開始となり、該当サービスを継続利用している限りは永年割引となります。詳細はソフトバンク窓口にてご確認ください。</li>
-                    <li>※ NURO 光ご契約者本人以外からのお申し込みの場合、専用同意書と家族確認書類が必要です。下記よりダウンロードをしていただき、契約者さまにご記入していただき、ソフトバンク窓口までご持参ください。<a href="">おうち割 光セット専用同意書</a></li>
+                    <li>※ NURO 光ご契約者本人以外からのお申し込みの場合、専用同意書と家族確認書類が必要です。下記よりダウンロードをしていただき、契約者さまにご記入していただき、ソフトバンク窓口までご持参ください。<br><a href="https://www.softbank.jp/mobile/set/common/pdf/campaigns/list/ouchiwari-hikari/ouchiwari-hikari_consent.pdf" target="_blank">おうち割 光セット専用同意書</a></li>
                     <li>※ おうち割 光セットのお申し込みは、「NURO 光 でんわ おうち割 光セット確認書」を弊社へご送付の上、NURO 光 でんわ開通後にソフトバンクショップにてお申し込みください。 NURO 光契約者様がソフトバンクショップにお越しいただき、お申し込みの際に『NURO 光 でんわ おうち割 光セット確認書』と『固定電話番号』の情報を申請していただきます様お願いいたします。 NURO 光 でんわのご契約者様ご本人以外からのお申し込みの場合、NURO 光 でんわご契約者様の同意書および、ご契約者様の家族であることを証明できる確認書類が必要となります。</li>
                     <li>※ 「NURO 光 でんわ おうち割 光セット確認書」はNURO 光 マイページよりダウンロードいただけます。iphone、Softbank スマートフォン、Softbank 3G（携帯電話）、iPad、タブレットのいずれかで指定のデータ（パケット）定額サービスにご加入いただく必要がございます。</li>
                     <li>※ 法人回線の方もお申し込み可能です。</li>
                     <li>※ NURO 光 でんわはソニーネットワークコミュニケーションズ株式会社がソフトバンクテレコム株式会社と提携して提供するサービスです。</li>
-                    <li>※ <b>「ソフトバンクの特典期間と内容は予告なく変更する場合があります。 詳細は<a href="">ソフトバンクホームページ</a>をご確認ください。」</b></li>
+                    <li>※ <b>「ソフトバンクの特典期間と内容は予告なく変更する場合があります。 詳細は<a href="https://www.softbank.jp/mobile/campaigns/list/ouchiwari-hikari/" target="_blank">ソフトバンクホームページ</a>をご確認ください。」</b></li>
                 </ul>
             </article>
-            <artcile>
+            <article>
                 <h3>NURO 光 でんわオプションサービス「おうち割 光セット」</h3>
                 <p>NURO 光 でんわとセットでご利用いただくことで、ご家族でお使いのソフトバンクのスマートフォン、3Gケータイ、またはiPadのご利用料金から最大毎月1,100円割引となるサービスです。</p>
                 <table>
@@ -79,25 +79,25 @@
                     <li>※ ウルトラギガモンスター定額50GB、データ定額5GB/20GBご利用でお申し込みの場合。割引金額は対象料金サービスにより異なります。</li>
                 </ul>
                 <h4>適用条件</h4>
-                <div>
+                <div class="box">
                     <dl>
                         <dt>NURO 光でんわ</dt>
-                        <dd><img src="" alt="NURO 光でんわ"></dd>
+                        <dd><img src="img/ouchi_img01.gif" alt="NURO 光でんわ"></dd>
                     </dl>
                     <div>+</div>
                     <dl>
                         <dt>ソフトバンク端末</dt>
-                        <dd><img src="" alt="ソフトバンク端末"></dd>
+                        <dd><img src="img/ouchi_img02.gif" alt="ソフトバンク端末"></dd>
                     </dl>
                     <div>+</div>
                     <dl>
                         <dt>ホワイトコール24</dt>
-                        <dd><img src="" alt="ホワイトコール24"></dd>
+                        <dd><img src="img/ouchi_img03.gif" alt="ホワイトコール24"></dd>
                     </dl>
                     <div>=</div>
                     <dl>
                         <dt>毎月1,100円割引★</dt>
-                        <dd><img src="" alt="毎月1,100円割引★"></dd>
+                        <dd><img src="img/ouchi_img04.gif" alt="毎月1,100円割引★"></dd>
                     </dl>
                 </div>
                 <h5>その他適用条件</h5>
@@ -110,8 +110,8 @@
                 </ul>
                 <h4>お申し込み方法</h4>
                 <p>下記の確認書を、確認書１ページに記載の宛先へご送付の上、NURO 光 でんわ開通後に最寄りのソフトバンクショップにてお申し込みいただくようお願い致します。</p>
-                <a href="">専用確認書のダウンロード</a>
-            </artcile>
+                <a href="https://www.nuro.jp/hikari/phone/ouchiwari_kakuninsho.pdf" target="_blank">専用確認書のダウンロード</a>
+            </article>
         </section>
 
         <section id="support">
