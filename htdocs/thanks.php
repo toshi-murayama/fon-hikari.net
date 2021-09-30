5f6d03bf37247eafe9cc847eb31c720a42e7c660
naru <scramask@gmail.com>
2021-09-30 15:26:13 +0900

    フリーダイブタグ更新 #140
    開始タグはGTMにて設定
    lptag.phpはフリーダイブに設置するようにお願いされたファイル

--- htdocs/thanks.php
+++ htdocs/thanks.php
@@ -85,21 +85,29 @@
 <noscript><img src="https://r.moshimo.com/af/r/result?p_id=3037&pc_id=7006&m_v=<?php print($_SESSION['affiOrderNumber']); ?>" width="1" height="1" alt=""></noscript>
 <!-- end もしも CV計測タグ 20201008 -->
 
-<!-- フリーダイブ 成果識別タグ 20201008 -->
+<!-- フリーダイブ 成果識別タグ 20210930 -->
 <script>
+(function(){
 var _CIDN = "cid";
+var _PLIDN = "plid";
+var _ACTN = "cid_auth_get_type";
+var _APTN = "plid_auth_get_type";
 var _PMTV = "5f7558aaacb8c";
 var _ARGSV = "<?php print($_SESSION['affiOrderNumber']); ?>";
-var _TRKU = "https://s8affi.net/asp/track.php?p=" + _PMTV + "&t=5f7558aa&args=" + _ARGSV;
+var _TRKU = "https://s8affi.net/track.php?p=" + _PMTV + "&args=" + _ARGSV;
 var _cks = document.cookie.split("; ");
-var _cidv = "";
-for(var i = 0; i < _cks.length; i++){ var _ckd = _cks[i].split( "=" ); if(_ckd[0] == "CL_" + _PMTV && _ckd[1].length > 1){ _cidv = _ckd[1]; break; }}
-if(!_cidv && localStorage.getItem("CL_" + _PMTV)){ _cidv = localStorage.getItem("CL_" + _PMTV); };
-if(_cidv){ _TRKU += "&" + _CIDN + "=" + _cidv };
-img = document.body.appendChild(document.createElement("img"));
-img.src = _TRKU;
+var _cidv = "", _plidv = "", _actv = "", _aptv = "";
+for(var i = 0; i < _cks.length; i++){ var _ckd = _cks[i].split("="); if(_ckd[0] == "CL_" + _PMTV && _ckd[1].length > 1){ _cidv = _ckd[1]; } if(_ckd[0] == "PL_" + _PMTV && _ckd[1].length > 1){ _plidv = _ckd[1]; } if(_ckd[0] == "ACT_" + _PMTV && _ckd[1].length > 1){ _actv = _ckd[1]; } if(_ckd[0] == "APT_" + _PMTV && _ckd[1].length > 1){ _aptv = _ckd[1]; } if(_cidv && _plidv && _actv && _aptv) break; }
+if(!_cidv && localStorage.getItem("CL_" + _PMTV)){ _cidv = localStorage.getItem("CL_" + _PMTV); _actv = "ls"; }
+if(_cidv){ _TRKU += "&" + _CIDN + "=" + _cidv; }
+if(!_plidv && localStorage.getItem("PL_" + _PMTV)){ _plidv = localStorage.getItem("PL_" + _PMTV); _aptv = "ls"; }
+if(_plidv){ _TRKU += "&" + _PLIDN + "=" + _plidv; }
+if(_actv){ _TRKU += "&" + _ACTN + "=" + _actv; }
+if(_aptv){ _TRKU += "&" + _APTN + "=" + _aptv; }
+var _xhr = new XMLHttpRequest(); _xhr.open("GET", _TRKU + "&t=5f7558aa"); _xhr.send();
+}());
 </script>
-<!-- end フリーダイブ 成果識別タグ 20201008 -->
+<!-- end フリーダイブ 成果識別タグ 20210930 -->
 
 <!-- レントラックス 成果識別タグ 20201008 -->
 <script type="text/javascript">
