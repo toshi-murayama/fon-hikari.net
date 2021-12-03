1006a9bd8c36a69bc46ad2d1f5a6d19110d41dcf
toshi-murayama <t-murayama@1onepiece.jp>
2021-12-03 13:02:33 +0900

    config.php の追加．

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use Monolog\Logger;
+
+// //////////////////////////////////////////////////////
+// // ログレベル： DEBUG, INFO. NOTICE, WARNING, ERROR,CRITICAL, ALERT, EMERGENCY 
+$DEBUG_LOG_LEVEL = Logger::INFO ;
+
+// ログディレクトリ．個別に設定できるが，同じディレクトリを指定しても可．
+$DEBUG_LOG_DIR = __DIR__ . '/log/';  // デバッグ用
+
+// //////////////////////////////////////////////////////
+// 管理者
+
+$SUPPORT_CLOUDBACKUP_EMAIL =[
+    'FROM' => ['support@fon-hikari.net' => 'クラウドバックアップ'],
+    'TO' => ['support@fon-hikari.net' => 'クラウドバックアップ'],
+];
