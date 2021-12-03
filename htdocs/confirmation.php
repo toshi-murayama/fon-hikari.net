075db76298224e57c950165ec21cb0c5c9575c53
toshi-murayama <t-murayama@1onepiece.jp>
2021-12-03 15:19:10 +0900

    <?php print の省略気泡への入れ替え．

--- htdocs/confirmation.php
+++ htdocs/confirmation.php
@@ -53,64 +53,64 @@
 				<li class="categories">
 					<dl>
 						<dt>氏名（姓）
-							<p><?php print $lastName; ?></p>
+							<p><?= $lastName; ?></p>
 						</dt>
 						<dd>氏名（名）
-							<p><?php print $firstName; ?></p>
+							<p><?= $firstName; ?></p>
 						</dd>
 					</dl>
 				</li>
 				<li class="categories">
 					<dl>
 						<dt>フリガナ（セイ）
-							<p><?php print $lastNameKana; ?></p>
+							<p><?= $lastNameKana; ?></p>
 						</dt>
 						<dd>フリガナ（メイ）
-							<p><?php print $firstNameKana; ?></p>
+							<p><?= $firstNameKana; ?></p>
 						</dd>
 					</dl>
 				</li>
-				<li class="categories">性別</li><li><p><?php print $sexString; ?></p></li>
-				<li class="categories">生年月日</li><li><p><?php print $birthday; ?></p></li>
-				<li class="categories">携帯番号</li><li><p><?php print $phoneNumber; ?></p></li>
-				<li class="categories">メールアドレス</li><li><p><?php print $mailAddress; ?></p></li>
-				<li class="categories">郵便番号</li><li><p><?php print $postalCode; ?></p></li>
-				<li class="categories">都道府県</li><li><p><?php print $installationPref; ?></p></li>
-				<li class="categories">市区町村</li><li><p><?php print $installationMunicipalities; ?></p></li>
-				<li class="categories">町名・丁目</li><li><p><?php print $installationTown; ?></p></li>
-				<li class="categories">番地・号</li><li><p><?php print $installationAddress; ?></p></li>
-				<li class="categories">建物名・部屋番号</li><li><p><?php print $installationBuilding; ?></p></li>
-				<li class="categories">物件の種類</li><li><p><?php print $homeTypeString; ?></p></li>
-				<li class="categories">所有形態</li><li><p><?php print $ownershipString; ?></p></li>
+				<li class="categories">性別</li><li><p><?= $sexString; ?></p></li>
+				<li class="categories">生年月日</li><li><p><?= $birthday; ?></p></li>
+				<li class="categories">携帯番号</li><li><p><?= $phoneNumber; ?></p></li>
+				<li class="categories">メールアドレス</li><li><p><?= $mailAddress; ?></p></li>
+				<li class="categories">郵便番号</li><li><p><?= $postalCode; ?></p></li>
+				<li class="categories">都道府県</li><li><p><?= $installationPref; ?></p></li>
+				<li class="categories">市区町村</li><li><p><?= $installationMunicipalities; ?></p></li>
+				<li class="categories">町名・丁目</li><li><p><?= $installationTown; ?></p></li>
+				<li class="categories">番地・号</li><li><p><?= $installationAddress; ?></p></li>
+				<li class="categories">建物名・部屋番号</li><li><p><?= $installationBuilding; ?></p></li>
+				<li class="categories">物件の種類</li><li><p><?= $homeTypeString; ?></p></li>
+				<li class="categories">所有形態</li><li><p><?= $ownershipString; ?></p></li>
 			</ul>
 			<h4>オプション</h4>
 			<ul class="form">
-				<li class="categories">光電話申込</li><li><p><?php print $telephoneApplicationString; ?></p></li>
+				<li class="categories">光電話申込</li><li><p><?= $telephoneApplicationString; ?></p></li>
 				<div <?php if ($telephoneApplication == "0") { ?> style="display:none"<?php } ?> >
-					<li class="categories">発番方法</li><li><p><?php print $numberingMethodString; ?></p></li>
+					<li class="categories">発番方法</li><li><p><?= $numberingMethodString; ?></p></li>
 				</div>
 				<div <?php if ($numberingMethod == "0") { ?> style="display:none" <?php } ?> >
-					<li class="categories">固定電話番号</li><li><p><?php print $fixedLine; ?></p></li>
+					<li class="categories">固定電話番号</li><li><p><?= $fixedLine; ?></p></li>
 				</div>
-				<li class="categories">リモートサポート</li><li><p><?php print $remortSupportString; ?></p></li>
-				<li class="categories">まとめてでんき</li><li><p><?php print $collectivelyElectricityString; ?></p></li>
-				<li class="categories">ひかりTV for NURO申込</li><li><p><?php print $hikariTVString; ?></p></li>
+				<li class="categories">リモートサポート</li><li><p><?= $remortSupportString; ?></p></li>
+				<li class="categories">まとめてでんき</li><li><p><?= $collectivelyElectricityString; ?></p></li>
+				<li class="categories">ひかりTV for NURO申込</li><li><p><?= $hikariTVString; ?></p></li>
 				<div <?php if ($hikariTV == "0") { ?> style="display:none"<?php } ?> >
-					<li class="categories">ひかりTV プラン</li><li><p><?php print $hikariTvPlanString; ?></p></li>
+					<li class="categories">ひかりTV プラン</li><li><p><?= $hikariTvPlanString; ?></p></li>
 				</div>
-				<li class="categories">カスペルスキーセキュリティー</li><li><p><?php print $kasperskySecurityString; ?></p></li>
-				<li class="categories">希望工事日</li><li><p><?php print nl2br($construction); ?></p></li>
+				<li class="categories">カスペルスキーセキュリティー</li><li><p><?= $kasperskySecurityString; ?></p></li>
+				<li class="categories">希望工事日</li><li><p><?= nl2br($construction); ?></p></li>
 			</ul>
 			<h4>入会書類郵送先</h4>
 			<ul class="form">
-				<li class="categories">入会書類郵送希望先</li><li><p><?php print $mailingDestinationString; ?></p></li>
+				<li class="categories">入会書類郵送希望先</li><li><p><?= $mailingDestinationString; ?></p></li>
 				<div <?php if ($mailingDestination == "0") { ?> style="display:none" <?php } ?> >
-					<li class="categories">郵便番号</li><li><p><?php print $mailingPostalCode; ?></p></li>
-					<li class="categories">都道府県</li><li><p><?php print $mailingPrefName; ?></p></li>
-					<li class="categories">市区町村</li><li><p><?php print $mailingMunicipalities; ?></p></li>
-					<li class="categories">町名・丁目</li><li><p><?php print $mailingTown; ?></p></li>
-					<li class="categories">番地・号</li><li><p><?php print $mailingAddress; ?></p></li>
-					<li class="categories">建物名・部屋番号</li><li><p><?php print $mailingBuilding; ?></p></li>
+					<li class="categories">郵便番号</li><li><p><?= $mailingPostalCode; ?></p></li>
+					<li class="categories">都道府県</li><li><p><?= $mailingPrefName; ?></p></li>
+					<li class="categories">市区町村</li><li><p><?= $mailingMunicipalities; ?></p></li>
+					<li class="categories">町名・丁目</li><li><p><?= $mailingTown; ?></p></li>
+					<li class="categories">番地・号</li><li><p><?= $mailingAddress; ?></p></li>
+					<li class="categories">建物名・部屋番号</li><li><p><?= $mailingBuilding; ?></p></li>
 				</div>
 			</ul>
 			<dl class="btn">
@@ -119,44 +119,44 @@
 			</dl>
 
 			<!-- postを設定 -->
-			<input type="hidden" value="<?php print h($applicationClassification); ?>" name="applicationClassification">
-			<input type="hidden" value="<?php print h($lastName); ?>" name="lastName">
-			<input type="hidden" value="<?php print h($firstName); ?>" name="firstName">
-			<input type="hidden" value="<?php print h($lastNameKana); ?>" name="lastNameKana">
-			<input type="hidden" value="<?php print h($firstNameKana); ?>" name="firstNameKana">
-			<input type="hidden" value="<?php print h($sex); ?>" name="sex">
-			<input type="hidden" value="<?php print h($birthday); ?>" name="birthday">
-			<input type="hidden" value="<?php print h($phoneNumber); ?>" name="phoneNumber">
-			<input type="hidden" value="<?php print h($fixedLine); ?>" name="fixedLine">
-			<input type="hidden" value="<?php print h($mailAddress); ?>" name="mailAddress">
-			<input type="hidden" value="<?php print h($postalCode); ?>" name="postalCode">
-			<input type="hidden" value="<?php print h($installationPref); ?>" name="installationPref">
-			<input type="hidden" value="<?php print h($installationMunicipalities); ?>" name="installationMunicipalities">
-			<input type="hidden" value="<?php print h($installationTown); ?>" name="installationTown">
-			<input type="hidden" value="<?php print h($installationAddress); ?>" name="installationAddress">
-			<input type="hidden" value="<?php print h($installationBuilding); ?>" name="installationBuilding">
-			<input type="hidden" value="<?php print h($ownership); ?>" name="ownership">
-			<input type="hidden" value="<?php print h($mailingDestination); ?>" name="mailingDestination">
-			<input type="hidden" value="<?php print h($mailingPostalCode); ?>" name="mailingPostalCode">
-			<input type="hidden" value="<?php print h($mailingPrefName); ?>" name="mailingPrefName">
-			<input type="hidden" value="<?php print h($mailingMunicipalities); ?>" name="mailingMunicipalities">
-			<input type="hidden" value="<?php print h($mailingTown); ?>" name="mailingTown">
-			<input type="hidden" value="<?php print h($mailingAddress); ?>" name="mailingAddress">
-			<input type="hidden" value="<?php print h($mailingBuilding); ?>" name="mailingBuilding">
-			<input type="hidden" value="<?php print h($telephoneApplication); ?>" name="telephoneApplication">
-			<input type="hidden" value="<?php print h($homeType); ?>" name="homeType">
-			<input type="hidden" value="<?php print h($numberingMethod); ?>" name="numberingMethod">
-			<input type="hidden" value="<?php print h($remortSupport); ?>" name="remortSupport">
-			<input type="hidden" value="<?php print h($collectivelyElectricity); ?>" name="collectivelyElectricity">
-			<input type="hidden" value="<?php print h($hikariTV); ?>" name="hikariTV">
-			<input type="hidden" value="<?php print h($kasperskySecurity); ?>" name="kasperskySecurity">
-			<input type="hidden" value="<?php print h($construction); ?>" name="construction">
-			<input type="hidden" value="<?php print h($hikariTvPlan); ?>" name="hikariTvPlan">
-            <input type="hidden" value="<?php print h($hikariTvPlanString); ?>" name="hikariTvPlanString">
-			<input type="hidden" value="<?php print h($hikariTvPlanApplication); ?>" name="hikariTvPlanApplication">
-			<input type="hidden" value="<?php print h($hikariTvPlanTuner); ?>" name="hikariTvPlanTuner">
-			<input type="hidden" value="<?php print h($affiOrderNumber); ?>" name="affiOrderNumber">
-			<input type="hidden" value="<?php print h($_SESSION['tk']); ?>" name="tk">
+			<input type="hidden" value="<?= h($applicationClassification); ?>" name="applicationClassification">
+			<input type="hidden" value="<?= h($lastName); ?>" name="lastName">
+			<input type="hidden" value="<?= h($firstName); ?>" name="firstName">
+			<input type="hidden" value="<?= h($lastNameKana); ?>" name="lastNameKana">
+			<input type="hidden" value="<?= h($firstNameKana); ?>" name="firstNameKana">
+			<input type="hidden" value="<?= h($sex); ?>" name="sex">
+			<input type="hidden" value="<?= h($birthday); ?>" name="birthday">
+			<input type="hidden" value="<?= h($phoneNumber); ?>" name="phoneNumber">
+			<input type="hidden" value="<?= h($fixedLine); ?>" name="fixedLine">
+			<input type="hidden" value="<?= h($mailAddress); ?>" name="mailAddress">
+			<input type="hidden" value="<?= h($postalCode); ?>" name="postalCode">
+			<input type="hidden" value="<?= h($installationPref); ?>" name="installationPref">
+			<input type="hidden" value="<?= h($installationMunicipalities); ?>" name="installationMunicipalities">
+			<input type="hidden" value="<?= h($installationTown); ?>" name="installationTown">
+			<input type="hidden" value="<?= h($installationAddress); ?>" name="installationAddress">
+			<input type="hidden" value="<?= h($installationBuilding); ?>" name="installationBuilding">
+			<input type="hidden" value="<?= h($ownership); ?>" name="ownership">
+			<input type="hidden" value="<?= h($mailingDestination); ?>" name="mailingDestination">
+			<input type="hidden" value="<?= h($mailingPostalCode); ?>" name="mailingPostalCode">
+			<input type="hidden" value="<?= h($mailingPrefName); ?>" name="mailingPrefName">
+			<input type="hidden" value="<?= h($mailingMunicipalities); ?>" name="mailingMunicipalities">
+			<input type="hidden" value="<?= h($mailingTown); ?>" name="mailingTown">
+			<input type="hidden" value="<?= h($mailingAddress); ?>" name="mailingAddress">
+			<input type="hidden" value="<?= h($mailingBuilding); ?>" name="mailingBuilding">
+			<input type="hidden" value="<?= h($telephoneApplication); ?>" name="telephoneApplication">
+			<input type="hidden" value="<?= h($homeType); ?>" name="homeType">
+			<input type="hidden" value="<?= h($numberingMethod); ?>" name="numberingMethod">
+			<input type="hidden" value="<?= h($remortSupport); ?>" name="remortSupport">
+			<input type="hidden" value="<?= h($collectivelyElectricity); ?>" name="collectivelyElectricity">
+			<input type="hidden" value="<?= h($hikariTV); ?>" name="hikariTV">
+			<input type="hidden" value="<?= h($kasperskySecurity); ?>" name="kasperskySecurity">
+			<input type="hidden" value="<?= h($construction); ?>" name="construction">
+			<input type="hidden" value="<?= h($hikariTvPlan); ?>" name="hikariTvPlan">
+            <input type="hidden" value="<?= h($hikariTvPlanString); ?>" name="hikariTvPlanString">
+			<input type="hidden" value="<?= h($hikariTvPlanApplication); ?>" name="hikariTvPlanApplication">
+			<input type="hidden" value="<?= h($hikariTvPlanTuner); ?>" name="hikariTvPlanTuner">
+			<input type="hidden" value="<?= h($affiOrderNumber); ?>" name="affiOrderNumber">
+			<input type="hidden" value="<?= h($_SESSION['tk']); ?>" name="tk">
 			<input type="hidden" name="confirmationSubmitFlag" value="1">
 			<input type="hidden" name="couponCode" value="<?= $couponCode ?>">
 		</form>
